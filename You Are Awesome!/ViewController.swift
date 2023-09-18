5008c838bf05f4ce46267da4c9cde85315c17e54
Clarence <csternagle@sbcglobal. net>
2023-09-18 09:45:15 -0400

    Section 2.9 You Are Awesome

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
 
     @IBOutlet var messageLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
+    
+    var imageNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      //   print("😎 ViewDidLoad has run!")
@@ -18,19 +21,33 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You Are Da Bomb!"
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = bombMessage
-            imageView.image = UIImage(named: "image2")
-        } else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
+        
+        print(imageNumber)
+        
+    //  the next two lines concatenate a Int to a string
+    //  let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 5 {
+            imageNumber = 0
         }
+        
+        
+        //        let awesomeMessage = "You Are Awesome!"
+        //        let greatMessage = "You Are Great!"
+        //        let bombMessage = "You Are Da Bomb!"
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text = greatMessage
+        //            imageView.image = UIImage(named: "image1")
+        //        } else if messageLabel.text == greatMessage {
+        //            messageLabel.text = bombMessage
+        //            imageView.image = UIImage(named: "image2")
+        //        } else {
+        //            messageLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "image0")
+        //        }
     }
     
 }
