1e55c17fa2d57af756b4dc551b123c35fa996fbf
Clarence <csternagle@sbcglobal. net>
2023-09-27 14:55:37 -0400

    Section 2.13 You Are Awesome

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet var messageLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
     
-    var imageNumber: Int = 0
-    var messageNumber: Int = 0
+    var imageNumber: Int = -1
+    var messageNumber: Int = -1
     let totalNumberOfImages = 4
     
     override func viewDidLoad() {
@@ -24,47 +24,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        var messageArray = ["You Are Awesome!",
+        let messageArray = ["You Are Awesome!",
                             "You Are Great",
                             "You Are Fantastic!",
                             "When the Genius Bar Needs Help, They Call You!",
                             "Fabulous? That's You!",
                             "You've got The Design Skills of Jony Ive",]
-        messageLabel.text =  messageArray[Int.random(in: 0...messageArray.count - 1)]
+        var newMessageNumber: Int
+        repeat {
+            newMessageNumber = Int.random(in: 0...messageArray.count - 1)
+            
+        } while messageNumber == newMessageNumber
+        
+        messageNumber = newMessageNumber
+        messageLabel.text =  messageArray[messageNumber]
 
-        imageView.image = UIImage(named: "image\(Int.random(in: 0...4))")
-    //    print(imageNumber)
         
-    //  the next two lines concatenate a Int to a string
-    //  let imageName = "image" + String(imageNumber)
-    //    let imageName = "image\(imageNumber)"
-           
-     
+       
         
-    // Removed one messageArray element without changing rest of code
-    //    var removeMessage = messageArray.remove(at: 4)
-    //    messageLabel.text = messageArray[imageNumber]
-    //    imageView.image = UIImage(named: imageName)
-    //    imageNumber = imageNumber + 1
-     //   if imageNumber == 5 {
-     //   if imageNumber == messageArray.count {
-      //      imageNumber = 0
-      //  }
+        var newImageNumber: Int
+        repeat {
+            
+           // print("Could put a print statement here")
+            
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+            
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
         
-        
-        //        let awesomeMessage = "You Are Awesome!"
-        //        let greatMessage = "You Are Great!"
-        //        let bombMessage = "You Are Da Bomb!"
-        //        if messageLabel.text == awesomeMessage {
-        //            messageLabel.text = greatMessage
-        //            imageView.image = UIImage(named: "image1")
-        //        } else if messageLabel.text == greatMessage {
-        //            messageLabel.text = bombMessage
-        //            imageView.image = UIImage(named: "image2")
-        //        } else {
-        //            messageLabel.text = awesomeMessage
-        //            imageView.image = UIImage(named: "image0")
-        //        }
     }
     
 }
