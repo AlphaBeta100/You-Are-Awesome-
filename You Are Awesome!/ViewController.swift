b3790bd4ac025c6367131baa97c01d6bc59b204a
Clarence <csternagle@sbcglobal. net>
2023-10-10 09:10:47 -0400

    Section 2.Sound Button  You Are Awesome

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -6,54 +6,94 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var messageLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
     
     var imageNumber: Int = -1
     var messageNumber: Int = -1
+    var soundNumber: Int = -1
     let totalNumberOfImages = 4
+    let totalNumberOfSounds = 6
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-     //   print("😎 ViewDidLoad has run!")
-     //   messageLabel.text = "Fabulous? That's You"
+        
+        //   print("😎 ViewDidLoad has run!")
+        //   messageLabel.text = "Fabulous? That's You"
     }
+        
+        @IBAction func messageButtonPressed(_ sender: UIButton) {
+            
+            func playSound(name: String) {
+                if let sound = NSDataAsset(name: name) {
+                    do {
+                        try   audioPlayer = AVAudioPlayer(data: sound.data)
+                        audioPlayer.play()
+                    } catch {
+                        print("🥲 ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object.")
+                    }
+                    
+                } else {
+                    print("🥲 ERROR: Could not read data from file sounds.")
+                }
+                
+            }
 
-    @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let messageArray = ["You Are Awesome!",
-                            "You Are Great",
-                            "You Are Fantastic!",
-                            "When the Genius Bar Needs Help, They Call You!",
-                            "Fabulous? That's You!",
-                            "You've got The Design Skills of Jony Ive",]
-        var newMessageNumber: Int
-        repeat {
-            newMessageNumber = Int.random(in: 0...messageArray.count - 1)
+            func nonRepeatingRandom(originalNumber: Int, upperLimit: Int) -> Int {
+                var newNumber: Int
+                repeat {
+                    newNumber = Int.random(in: 0...upperLimit)
+                    
+                } while originalNumber == newNumber
+                return newNumber
+            }
             
-        } while messageNumber == newMessageNumber
-        
-        messageNumber = newMessageNumber
-        messageLabel.text =  messageArray[messageNumber]
+            let messageArray = ["You Are Awesome!",
+                                "You Are Great",
+                                "You Are Fantastic!",
+                                "When the Genius Bar Needs Help, They Call You!",
+                                "Fabulous? That's You!",
+                                "You've got The Design Skills of Jony Ive",]
+            
+            messageNumber = nonRepeatingRandom(originalNumber: messageNumber, upperLimit: messageArray.count - 1)
+            
+            messageLabel.text =  messageArray[messageNumber]
+            
+    imageNumber = nonRepeatingRandom(originalNumber: imageNumber, upperLimit: totalNumberOfImages - 1)
+            imageView.image = UIImage(named: "image\(imageNumber)")
 
-        
-       
-        
-        var newImageNumber: Int
-        repeat {
+    soundNumber = nonRepeatingRandom(originalNumber: soundNumber, upperLimit: totalNumberOfSounds - 1)
             
-           // print("Could put a print statement here")
             
+ /*           var newImageNumber: Int
+            repeat {
+                // print("Could put a print statement here")
             newImageNumber = Int.random(in: 0...totalNumberOfImages)
+            } while imageNumber == newImageNumber
+            imageNumber = newImageNumber
+            imageView.image = UIImage(named: "image\(imageNumber)")
+  
             
-        } while imageNumber == newImageNumber
-        imageNumber = newImageNumber
-        imageView.image = UIImage(named: "image\(imageNumber)")
+            var newSoundNumber: Int
+            repeat {
+                // print("Could put a print statement here")
+            newSoundNumber = Int.random(in: 0...totalNumberOfSounds - 1)
+            } while soundNumber == newSoundNumber
+            soundNumber = newSoundNumber
+            print("*** The new Sound Number is \(soundNumber)")
+*/
+            playSound(name: "sound\(soundNumber)")
+            
+            
+        }
         
     }
     
-}
 
